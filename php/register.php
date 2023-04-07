612a589166dd9619da616e4bb83aaecb73398bf1
Parama02 <99189639+Parama02@users.noreply.github.com>
2023-04-08 00:51:48 +0530

    Initial commit

---
+++ php/register.php
@@ -0,0 +1,56 @@
+<?php
+
+$email  = $_POST['email'];
+$mobile_no  = $_POST['mobile_no'];
+$password = $_POST['password'];
+
+
+
+
+if (!empty($email) || !empty($mobile_no) || !empty($password))
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "miniproject";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT email From register Where email = ? Limit 1";
+  $INSERT = "INSERT Into register (email ,mobile_no, password )values(?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("sis", $email,$mobile_no,$password);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
